27f3c1c4a074c229abf3f77d0172e96310c601cf
George Kim <ggkim0614@gmail.com>
2024-03-26 23:13:49 -0700

    removed node_modules

--- 05/Transform objects/src/script.js
+++ 05/Transform objects/src/script.js
@@ -50,32 +50,4 @@ mesh.scale.set(2, 0.25, 1);
 // mesh.scale.y = 0.25;
 // mesh.scale.z = 1;
 
-// Rotation
-group.rotation.reorder('YXZ');
-group.rotation.x = Math.PI * 0.25;
-group.rotation.y = Math.PI * 0.25;
-
-/**
- * Sizes
- */
-const sizes = {
-	width: 800,
-	height: 600,
-};
-
-/**
- * Camera
- */
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
-
-camera.position.z = 3;
-scene.add(camera);
-
-/**
- * Renderer
- */
-const renderer = new THREE.WebGLRenderer({
-	canvas: canvas,
-});
-renderer.setSize(sizes.width, sizes.height);
-renderer.render(scene, camera);
+/
